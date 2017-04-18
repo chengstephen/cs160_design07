e931beac70dcdedc9d07e14f1ee2b15ab15b1e13
Jonathan Tu <Jonathan Tu>
2017-04-18 10:29:12 -0700

    updating

--- app/src/main/java/com/projects/cs160_design07/MainActivity.java
+++ app/src/main/java/com/projects/cs160_design07/MainActivity.java
@@ -1,5 +1,7 @@
 package com.projects.cs160_design07;
 
+// Homepage screen!  This activity will run our news feed and act as our app's homepage.
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
