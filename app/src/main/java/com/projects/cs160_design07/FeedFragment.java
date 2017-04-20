f7cb21f71bc9c0289927081a61996c0066e8cd48
Jonathan Tu <Jonathan Tu>
2017-04-20 10:44:27 -0700

    fixed minor bugs, started Fragments

--- app/src/main/java/com/projects/cs160_design07/FeedFragment.java
+++ app/src/main/java/com/projects/cs160_design07/FeedFragment.java
@@ -1,10 +1,18 @@
 package com.projects.cs160_design07;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by cstep on 4/18/2017.
  */
 
 public class FeedFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_main, container, false);
+    }
 }
