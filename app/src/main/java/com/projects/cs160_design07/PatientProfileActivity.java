ca77dba25bef650c1f336f33c1d8d59f5d07e0f9
cs61b-acd <christinestraub@berkeley.edu>
2017-04-25 02:09:31 -0700

    patient profile done

---
+++ app/src/main/java/com/projects/cs160_design07/PatientProfileActivity.java
@@ -0,0 +1,13 @@
+package com.projects.cs160_design07;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PatientProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.patientprofile);
+    }
+}
