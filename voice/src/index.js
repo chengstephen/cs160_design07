31d8d8c9bacb02cc2a00a903e658130dfec331b5
Andrew Dildy <a.dildy@berkeley.edu>
2017-04-25 20:39:22 -0700

    updates

--- voice/src/index.js
+++ voice/src/index.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var APP_ID = '';
+var APP_ID = 'amzn1.ask.skill.7195510a-6126-40f7-8013-dccd7118bc12';
 var Alexa = require('alexa-sdk');
+var https = require('https');
 
 var STATES = {
     MAIN: '_MAINMODE',
@@ -15,34 +16,39 @@ var time = new Date();
 var languageString = {
     'en': {
         'translation': {
-            'WELCOME': "baymax here, i can help with urgent problems, scheduling, or any special requests. ",
-            'NEED_HELP': "what do you need help with?",
+            'WELCOME': "nurse assistant here, i can help with urgent problems, scheduling, or any special requests. ",
+            'NEED_HELP': "what can i help you with today?",
             'MAIN_HELP': "to get help, you can say 'urgent problem', 'scheduling', or 'special request'",
+
             'URGENT_PROMPT': "okay, what is your problem? if it's an emergency say, 'emergency'.",
             'EMERGENCY_RESPONSE': "okay, medical staff have been notified and will be arriving immediately",
-            'READ_REQUEST': "okay, your request is '%s'. the nurse will be here shortly",
-            'URGENT_HELP': "for emergencies say 'emergency', for other urgent help, just tell me what you need",
+            'READ_REQUEST': "okay, your request is, '%s'. the nurse will be here shortly",
+            'URGENT_HELP': "for emergencies say 'emergency', for other urgent help, just tell me what you need. return to the main menu by saying, main menu.",
+
             'SCHEDULE_PROMPT': "to hear the next item on your schedule, say next",
             'SCHEDULE': [
                             {
-                                'date': (new Date()).setMinutes(time.getMinutes() + 15),
-                                'appointment': 'take 10 milligrams of aspirin'
+                                'Time': (new Date()).setMinutes(time.getMinutes() + 15),
+                                'Appointment': 'take 10 milligrams of aspirin'
                             },
                             {
-                                'date': (new Date()).setMinutes(time.getMinutes() + 30),
-                                'appointment': 'blood pressure reading with nurse'
+                                'Time': (new Date()).setMinutes(time.getMinutes() + 30),
+                                'Appointment': 'blood pressure reading with nurse'
                             },
                             {
-                                'date': (new Date()).setMinutes(time.getMinutes() + 60),
-                                'appointment': 'lunch'
+                                'Time': (new Date()).setMinutes(time.getMinutes() + 60),
+                                'Appointment': 'lunch'
                             }
                         ],
             'READ_SCHEDULE': "%s in %s minutes",
             'END_SCHEDULE': "there are no more items in your schedule today. to hear your schedule again, just say next.",
             'NO_PREVIOUS': "there's no previous item, please say next to hear the next item on your schedule.",
-            'REQUEST_HELP': "",
-            'TRY_AGAIN': "i didn't quite catch that, could you repeat it?"
+            'SCHEDULE_HELP': "to hear the next item on your schedule, say next. return to the main menu by saying, main menu.",
 
+            'REQUEST_HELP': "return to the main menu by saying, main menu.",
+
+            'TRY_AGAIN': "i didn't quite catch that, could you repeat it?",
+            'GOODBYE': "see you next time"
         }
     }
 };
@@ -68,9 +74,9 @@ var newSessionHandlers = {
         this.emitWithState('UrgentIntent');
     },
 
-    'SchedulingIntent': function () {
+    'ScheduleIntent': function () {
         this.handler.state = STATES.MAIN;
-        this.emitWithState('SchedulingIntent');
+        this.emitWithState('ScheduleIntent');
     },
 
     'RequestIntent': function () {
@@ -99,7 +105,7 @@ var mainStateHandlers = Alexa.CreateStateHandler(STATES.MAIN, {
     },
 
     'ScheduleIntent': function () {
-        this.handler.state = STATES.SCHEDULING;
+        this.handler.state = STATES.SCHEDULE;
         this.emitWithState('start');
     },
 
@@ -110,6 +116,10 @@ var mainStateHandlers = Alexa.CreateStateHandler(STATES.MAIN, {
 
     'HelpIntent': function () {
         this.emit(':ask', this.t('MAIN_HELP'));
+    },
+
+    'ExitIntent': function () {
+        this.emit(':tell', this.t('GOODBYE'));
     },
 
     'Unhandled': function () {
@@ -129,7 +139,11 @@ var urgentStateHandlers = Alexa.CreateStateHandler(STATES.URGENT, {
         var newRequest = this.event.request.intent.slots.Request;
         if (newRequest && newRequest.value) {
             newRequest = newRequest.value.toLowerCase();
-            this.emit(':tell', this.t('READ_REQUEST', newRequest));
+            var that = this;
+            postUrgentRequest(newRequest, function() {
+                console.log('sentinel');
+                that.emit(':tell', that.t('READ_REQUEST', newRequest));
+            });
         } else {
             this.emit(':ask', this.t('NO_PREVIOUS'));
         }
@@ -145,7 +159,7 @@ var urgentStateHandlers = Alexa.CreateStateHandler(STATES.URGENT, {
     },
 
     'ScheduleIntent': function () {
-        this.handler.state = STATES.SCHEDULING;
+        this.handler.state = STATES.SCHEDULE;
         this.emitWithState('start');
     },
 
@@ -158,6 +172,10 @@ var urgentStateHandlers = Alexa.CreateStateHandler(STATES.URGENT, {
         this.emit(':ask', this.t('URGENT_HELP'));
     },
 
+    'ExitIntent': function () {
+        this.emit(':tell', this.t('GOODBYE'));
+    },
+
     'Unhandled': function () {
         this.emitWithState('HelpIntent');
     }
@@ -166,39 +184,43 @@ var urgentStateHandlers = Alexa.CreateStateHandler(STATES.URGENT, {
 
 var scheduleStateHandlers = Alexa.CreateStateHandler(STATES.SCHEDULE, {
     'start': function (newSession) {
-        this.attributes[scheduleCount] = 0;
+        this.attributes['scheduleCount'] = -1;
         var speechOutput = '';
         speechOutput += this.t('SCHEDULE_PROMPT');
         this.emit(':ask', speechOutput);
     },
 
+    'readAppointment': function (index) {
+        var schedule = this.t('SCHEDULE');
+        var i = schedule[index];
+        var diff = i.Time - new Date();
+        var minutes = Math.floor((diff / 1000) / 60);
+        this.emit(':ask', this.t('READ_SCHEDULE', i.Appointment, minutes));
+    },
+
     'NextIntent': function () {
         var schedule = this.t('SCHEDULE');
-        var scheduleCount = this.attributes[scheduleCount];
-        if (scheduleCount == schedule.length) {
-            this.attributes[scheduleCount] = 0;
+        if (this.attributes['scheduleCount'] == schedule.length) {
+            this.attributes['scheduleCount'] = 0;
             this.emit(':ask', 'END_SCHEDULE');
-        } else if (schedule[scheduleCount][0] - new Date() <= 0) {
-            this.attributes[scheduleCount]++;
+        } else if (this.attributes['scheduleCount'] >= 0 && schedule[this.attributes['scheduleCount']].Time - new Date() <= 0) {
+            this.attributes['scheduleCount']++;
             this.emitWithState('NextIntent');
         } else {
-            this.attributes[scheduleCount]++;
-            var i = schedule[scheduleCount];
-            var ms = i[0].getMinutes() - new Date();
-            var minutes = Math.floor((ms / 1000) / 60);
-            this.emit(':ask', this.t('READ_APPOINTMENT', i[1], minutes));
+            this.attributes['scheduleCount']++;
+            this.emitWithState('readAppointment', this.attributes['scheduleCount']);
         }
     },
 
     'StartOverIntent': function () {
-        this.attributes[scheduleCount] = 0;
+        this.attributes['scheduleCount'] = -1;
         this.emitWithState('NextIntent');
     },
 
     'PreviousIntent': function () {
-        if (this.attributes[scheduleCount] > 0) {
-            this.attributes[scheduleCount]--;
-            this.emitWithState('NextIntent');
+        if (this.attributes['scheduleCount'] > 0) {
+            this.attributes['scheduleCount']--;
+            this.emitWithState('readAppointment', this.attributes['scheduleCount']);
         } else {
             this.emit(':ask', this.t('NO_PREVIOUS'))
         }
@@ -220,7 +242,11 @@ var scheduleStateHandlers = Alexa.CreateStateHandler(STATES.SCHEDULE, {
     },
 
     'HelpIntent': function () {
-        this.emit(':ask', this.t('SCHEDULE_PROMPT'));
+        this.emit(':ask', this.t('SCHEDULE_HELP'));
+    },
+
+    'ExitIntent': function () {
+        this.emit(':tell', this.t('GOODBYE'));
     },
 
     'Unhandled': function () {
@@ -258,12 +284,46 @@ var requestStateHandlers = Alexa.CreateStateHandler(STATES.REQUEST, {
         this.emit(':ask', this.t('REQUEST_HELP'));
     },
 
+    'ExitIntent': function () {
+        this.emit(':tell', this.t('GOODBYE'));
+    },
+
     'Unhandled': function () {
         this.emitWithState('HelpIntent');
     }
 
 });
 
+function postUrgentRequest(request, callback) {
+    var timeStamp = (new Date()).getTime();
+    console.log(timeStamp);
+    var postData = {
+        Item: {
+            "Emergency": request,
+            "Time": timeStamp
+        },
+        TableName: "Emergencies"
+    };
+
+    var postOptions = {
+        host:  'a2owklu6i1.execute-api.us-east-1.amazonaws.com',
+        port: '443',
+        path: '/prod/EmergenciesUpdate',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    var postReq = https.request(postOptions, function(res) {
+        callback();
+    });
+
+    postReq.write(JSON.stringify(postData));
+    postReq.end();
+}
+
 function sortByDateHelper(a, b) {
     return a['date'] - b['date'];
 }
+
