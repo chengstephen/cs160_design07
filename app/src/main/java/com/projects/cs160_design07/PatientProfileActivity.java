81d550e6c7b0a6b152607954afe6003cd37dc2b7
S C <S C>
2017-05-08 17:23:29 -0700

    Fixed some things on the patient profile page

--- app/src/main/java/com/projects/cs160_design07/PatientProfileActivity.java
+++ app/src/main/java/com/projects/cs160_design07/PatientProfileActivity.java
@@ -2,6 +2,7 @@ package com.projects.cs160_design07;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class PatientProfileActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class PatientProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.patientprofile);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
